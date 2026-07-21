eeaabbf560d239d2ec893ec6eef8636fe64cb448
Roomora User <roomora@example.com>
2026-07-21 02:51:49 +0000

    Fix media serving in production and fix flexbox collapsing property cards

--- staymatch/urls.py
+++ staymatch/urls.py
@@ -64,6 +64,10 @@ urlpatterns = [
     path('profile/', lambda request: redirect('/accounts/profile/')),
 ]
 
+from django.urls import re_path
+from django.views.static import serve
+urlpatterns += [
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+]
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
