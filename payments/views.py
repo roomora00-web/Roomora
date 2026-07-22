dab95201e9545077109be89abad0db85ae3d85d8
Roomora User <roomora@example.com>
2026-07-22 19:10:41 +0000

    Fix NameError import for redirect in payments/views.py

--- payments/views.py
+++ payments/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from django.http import JsonResponse, HttpResponse
 from django.views import View
 from django.views.decorators.csrf import csrf_exempt
