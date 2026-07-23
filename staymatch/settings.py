fe538bafcd8a2cb149da5365c5805d67646dc0de
Roomora User <roomora@example.com>
2026-07-23 15:09:28 +0000

    Fix Neon DB connection timeout errors by setting conn_max_age to 0

--- staymatch/settings.py
+++ staymatch/settings.py
@@ -110,7 +110,7 @@ CHANNEL_LAYERS = {
 DATABASES = {
     'default': dj_database_url.config(
         default=os.environ.get('DATABASE_URL', f'sqlite:///{BASE_DIR / "db.sqlite3"}'),
-        conn_max_age=600,
+        conn_max_age=0,
         conn_health_checks=True,
     )
 }
